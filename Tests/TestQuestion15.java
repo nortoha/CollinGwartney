0f39841b0496728e91ea01ecbb842239de5e0c54
Big Chuggus <57680372+BigChuggus@users.noreply.github.com>
2019-12-12 12:18:27 -0600

    TestQuestion15
    TestQuestion15

---
+++ Tests/TestQuestion15.java
@@ -0,0 +1,16 @@
+
+public class TestQuestion15 {
+	
+	public static void main(String[] args) {
+		
+		int numTimes=5; {
+			String TestQuestion = "1 1 1 1 1"; 
+					
+			for(int counter=0; counter < numTimes; counter++) 
+				System.out.println(TestQuestion);
+		}			
+			
+			
+	}		
+			
+}
